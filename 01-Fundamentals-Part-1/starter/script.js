40d9dbd478409e690c51e9909f9333ed97afdebb
Peter <petelc1966@hotmail.com>
2022-05-30 04:23:30 -0400

    Section 2 Lecture 16

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,80 @@
+// const country = 'United States of America';
+// const continent = 'North America';
+// const population = 331893745;
+
+/**
+ * @author Peter Carroll
+ */
+// console.log(
+//   `I live in the ${country} which is in the continent of ${continent} with a population of ${population}`
+// );
+
+/**
+ * * DataTypes
+ *
+ * ? Primitives
+ * > Number
+ * > String
+ * > Boolean
+ *
+ * > Undefined
+ * > Null
+ * > Symbol (ES2015) unique and cannot be changed
+ * > BigInt (ES2020) Integers that are too large for the number type
+ *
+ * ! Dynamic typing - means you do not have to manually define the type of a variable
+ */
+
+/**
+ * * Operators
+ *
+ * > ** means to the power of so 2 ** 3 is 2 to the power of 3
+ *
+ */
+
+// // > CHALLENGE #1
+// let markWeight = 78;
+// let markHeight = 1.69;
+// let johnWeight = 95;
+// let johnHeight = 1.95;
+// let markHigherBMI = false;
+// let johnHigherBMI = false;
+
+// let markBMI = markWeight / (markHeight * markHeight);
+// let johnBMI = johnWeight / (johnHeight * johnHeight);
+
+// console.log('Marks BMI', markBMI.toFixed(2));
+// console.log("John's BMI", johnBMI.toFixed(2));
+
+// if (markBMI > johnBMI) {
+//   markHigherBMI = true;
+//   console.log(`Mark\s BMI is higher than John\'s`);
+// } else if (johnBMI > markBMI) {
+//   johnHigherBMI;
+//   console.log(`John\s BMI is higher than Mark\'s`);
+// } else {
+//   console.log(`Mark\s BMI is the same as John\'s`);
+// }
+
+// markWeight = 95;
+// markHeight = 1.88;
+// johnWeight = 85;
+// johnHeight = 1.76;
+// markHigherBMI = false;
+// johnHigherBMI = false;
+
+// markBMI = markWeight / (markHeight * markHeight);
+// johnBMI = johnWeight / (johnHeight * johnHeight);
+
+// console.log("Mark's NEW BMI", markBMI.toFixed(2));
+// console.log("John's NEW BMI", johnBMI.toFixed(2));
+
+// if (markBMI > johnBMI) {
+//   markHigherBMI = true;
+//   console.log(`Mark\s NEW BMI is higher than John\'s`);
+// } else if (johnBMI > markBMI) {
+//   johnHigherBMI;
+//   console.log(`John\s NEW BMI is higher than Mark\'s`);
+// } else {
+//   console.log(`Mark\s NEW BMI is the same as John\'s`);
+// }
